d0a69c19f6aa192bd8374f6a6f16725090a24093
turgutaga-sketch <turgutaga@me.com>
2026-07-20 02:04:23 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+/* AutoBüro · Verbindung zur Datenbank */
+window.AUTOBURO_CONFIG = {
+  supabaseUrl: "https://vnlcniulwrtlvmchayln.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZubGNuaXVsd3J0bHZtY2hheWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0Njc5MTQsImV4cCI6MjEwMDA0MzkxNH0.bsMZcNkyjjI2bP5eVf9lju44Utnrjhljz6F3cvYVC9U"
+};
